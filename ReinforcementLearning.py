f6173578bb7289b98047d88786ea30d08fc41c64
Michael <mlyonsru@gmail.com>
2017-12-10 10:11:41 -0700

    finished up algo

--- ReinforcementLearning.py
+++ ReinforcementLearning.py
@@ -1,4 +1,5 @@
 import random
+from Channel import Channel
 
 
 class ReinforcementLearning:
@@ -6,68 +7,75 @@ class ReinforcementLearning:
         self.Q = {}
         self.data = [[]]
 
-    def update_Q(self, time, action, next_time, next_action, interference):
-        self.Q[(time, action)] = interference + self.Q[(next_time, next_action)]
+    def check_optimal(self, time, move, learning_rate):
+        if move is not self.get_Qmax(time):
+            self.Q[(time, move)] *= learning_rate
+        else:
+            self.Q[(time, move)] *= (1 - learning_rate) + 1
 
     def get_channels(self, time):
         return self.data[time]
 
-    def action(self, epsilon, time):
+    def get_move(self, epsilon, time):
         alpha = random.random()
-        channels = self.get_channels(time)
+        next_channels = self.get_channels(time + 1)
 
         if alpha < epsilon:
-            return random.choice(channels)
+            return random.choice(next_channels)
         else:
-            return self.get_Qmin(time)
+            return self.get_Qmax(time)
 
-    def train(self, epsilon, epsilon_decay_factor, duration):
-
-        for time in range(duration):
+    def train(self, epsilon, epsilon_decay_factor, duration, iterations, learning_rate):
+        for _ in range(iterations):
             epsilon *= epsilon_decay_factor
-            action = self.action(epsilon, time)
-            next_time = time + 1
+            print(epsilon)
 
-            if next_time < duration:
-                next_action = self.action(epsilon, next_time)
-                self.update_Q(time, action, next_time, next_action)
+            for time in range(duration):
+                next_time = time + 1
 
-        pass
+                if next_time < duration:
+                    self.initialize_Q_time(time)
+                    move = self.get_move(epsilon, time)
+                    self.check_optimal(time, move, learning_rate)
+
 
     def test(self):
         pass
 
-    def get_Qmin(self, time):  # todo handle end of q table border case
+    def initialize_Q_time(self, time):
         next_channels = self.get_channels(time + 1)
-        lowest_reinforcement = 1
+
+        for next_channel in next_channels:
+            if self.Q.get((time, next_channel), -1) == -1:
+                self.Q[(time, next_channel)] = 100.0
+
+    def get_Qmax(self, time):  # todo handle end of q table border case
+        next_channels = self.get_channels(time + 1)
+        highest_reinforcement = 0
         optimal_channel = next_channels[0]
 
         for next_channel in next_channels:
             reinforcement = self.Q.get((time, next_channel))
-            if reinforcement < lowest_reinforcement:
+            if reinforcement > highest_reinforcement:
                 optimal_channel = next_channel
-                lowest_reinforcement = reinforcement
+                highest_reinforcement = reinforcement
 
         return optimal_channel
 
 
-def to_tuple(time, channel, next_channel):
-    return tuple(((time, channel), next_channel))
+data = []
 
-
-q = {}
-data = [[]]
-
-for i in range (10000):
+for i in range(10000):
+    round = []
     for j in range(10):
-        channel = 'c' + str(j + 1)
-        interference = random.random()
+        channel = Channel()
+        channel.interference = random.random()
+        channel.name = 'c' + str(j) + 't' + str(i)
+        round.append(channel)
+    data.append(round)
 
 
-
-q = {(0, 'c1'): 0.56, (0, 'c2'): 0.3}
-data = [['c1', 'c2'], ['c1', 'c2'], ['c1', 'c2']]
 a = ReinforcementLearning()
-a.Q = q
 a.data = data
-print(a.get_Qmin(0))
+a.train(0.999, 0.999, 10000, 100, 0.95)
+print(a.Q)

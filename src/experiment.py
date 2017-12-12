abc734bdcd7a21fe4157174cd7e734ad889a6222
Michael <mlyonsru@gmail.com>
2017-12-11 18:21:50 -0700

    added experiment structure:

--- src/experiment.py
+++ src/experiment.py
@@ -1,1 +1,17 @@
+from src.ReinforcementLearning import *
+from src.Channel import Channel
 
+
+def run_experiment(transmission_length, channel_length, epsilon, epsilon_decay_rate, iterations, learning_rate):
+    channel = Channel()
+    transmission_matrix = [[Channel() for y in range(channel_length)] for x in range(transmission_length)]
+    channel.fillChannelTemplate(transmission_matrix)
+    a = ReinforcementLearning()
+    a.data = transmission_matrix
+    a.train(epsilon, epsilon_decay_rate, iterations, learning_rate)
+
+    channel.fillChannelTemplate(transmission_matrix)
+
+    performance = a.test(transmission_matrix)
+
+    print(performance * 100)
